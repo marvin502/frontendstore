25f48b5d0b001226bae095e0b57a841f8a3a258d
marvin502 <62572934+marvin502@users.noreply.github.com>
2022-08-18 11:47:32 -0600

    Update nosotros.php

--- nosotros.php
+++ nosotros.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FrontEnd Store</title>
-    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="normalize.css">
     <link href="https://fonts.googleapis.com/css2?family=Ibarra+Real+Nova:ital,wght@0,400;0,500;1,400&family=Krub:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header class="header">
